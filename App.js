01467de07f10412c1470f15db34847e9c4378de9
Yogeshwar Saini <144199808+yogeshwarsaini@users.noreply.github.com>
2026-09-17 22:48:55 +0530

    Update title in App.js to include 'azure vm'

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ function App() {
         fontSize: '22px',
         fontWeight: 'bold'
       }}>
-        📚 Library Management System
+        📚 Library Management System - azure vm 
       </div>
 
       {view === 'list' ? (
